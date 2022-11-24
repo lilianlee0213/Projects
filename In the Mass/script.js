53fa29ee20af97b39c254b82e45b955e7bf66f3c
Lilian Lee <lilianlee0213@gmail.com>
2022-11-24 01:52:48 -0500

    Add contents and styles

--- In the Mass/script.js
+++ In the Mass/script.js
@@ -1,6 +1,7 @@
 const navOpen = document.querySelector('.nav-open');
 const closeBtn = document.querySelector('.nav-close');
 const navToggle = document.querySelectorAll('.nav-toggle');
+const navLinks = document.querySelectorAll('.links li');
 
 navToggle.forEach((element) => {
 	navOpen.addEventListener('click', () => {
@@ -10,3 +11,27 @@ navToggle.forEach((element) => {
 		element.classList.remove('show-sidebar');
 	});
 });
+const mouseCursor = document.querySelector('.mouse-cursor');
+console.log(mouseCursor);
+window.addEventListener('mousemove', cursor);
+function cursor(e) {
+	mouseCursor.style.top = e.pageY + 'px';
+	mouseCursor.style.left = e.pageX + 'px';
+}
+navLinks.forEach((link) => {
+	link.addEventListener('mouseover', () => {
+		mouseCursor.classList.add('link-grow');
+		link.classList.add('hovered-link');
+	});
+	link.addEventListener('mouseleave', () => {
+		mouseCursor.classList.remove('link-grow');
+		link.classList.remove('hovered-link');
+	});
+});
+// const cursorChild = cursorParent.children[0];
+
+// window.addEventListener('mousedown', mousedown);
+// window.addEventListener('mouseup', mouseup);
+
+// function mousedown(e) {}
+// function mouseup(e) {}
